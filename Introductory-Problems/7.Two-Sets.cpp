5271228ff5027d84495e879e39b0382d663adb44
Minh Tam Phan <phanminhtam247@gmail.com>
2021-03-11 22:24:38 +0700

    add test case

--- Introductory-Problems/7.Two-Sets.cpp
+++ Introductory-Problems/7.Two-Sets.cpp
@@ -1,30 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	long long n, sum;
-	cin >> n;
-	sum = n * (n + 1) / 2;
-	if (sum % 2) cout << "No";
-	else {
-		sum /= 2;
-		cout << "YES\n";
-		vector<long long> s1, s2;
-		
-		while(n > 0) {
-			if(sum - n >= 0) {
-				sum -= n;
-				s1.push_back(n);
-			} else {
-				s2.push_back(n);
-			}
-			n --;
-		}	
-		cout << s1.size() << endl;
-		for(auto e : s1) cout << e << " ";
-		cout << "\n" << s2.size() << endl;
-		for(auto e : s2) cout << e << " ";	
-	}
-	return 0;
-}
+    long long n, sum;
+    cin >> n;
+    sum = n * (n + 1) / 2;
+    if (sum % 2)
+        cout << "NO\n";
+    else {
+        sum /= 2;
+        cout << "YES\n";
+        vector<long long> s1, s2;
 
+        while (n > 0) {
+            if (sum - n >= 0) {
+                sum -= n;
+                s1.push_back(n);
+            } else {
+                s2.push_back(n);
+            }
+            n--;
+        }
+        cout << s1.size() << endl;
+        for (auto e : s1) cout << e << " ";
+        cout << "\n"
+             << s2.size() << endl;
+        for (auto e : s2) cout << e << " ";
+    }
+    return 0;
+}
--- Introductory-Problems/8.Bit-Strings.cpp
+++ Introductory-Problems/8.Bit-Strings.cpp
@@ -1,25 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-long long pow(int a, int p, long long mod) {
-	if (p == 0) return 1;
-	long long res = pow(a, p/2, mod);
-	if (p % 2) return res * res * a;
-	else return res * res;
+long long pow(int a, long long p, long long mod) {
+    if (p == 0) return 1;
+    long long res = pow(a, p / 2, mod);
+    if (p % 2)
+        return (((res * res) % mod) * a) % mod;
+    else
+        return (res * res) % mod;
 }
 
 int main() {
-	int n;
-	long long mod = 10e7;
-	cin >> n;
-	long long res = 1, a = 2;
-//	while(n > 0) {
-//		if (n % 2) res = (res * a) % mod;
-//		a = (a * a) % mod;
-//		n /= 2;
-//	}
+    long long mod = 1000000007, n;
+    cin >> n;
+    long long res = 1, a = 2;
+    //	while(n > 0) {
+    //		if (n % 2) res = (res * a) % mod;
+    //		a = (a * a) % mod;
+    //		n /= 2;
+    //	}
 
-	res = pow(a, n, mod);
-	cout << res << endl;
-	return 0;
+    res = pow(a, n, mod);
+    cout << res << endl;
+    return 0;
 }
